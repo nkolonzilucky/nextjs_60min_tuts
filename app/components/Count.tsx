f741e2d920edc7c59e3b62379f2c09c961f547c2
Lucky Nkolonzi <35879963+nkolonzilucky@users.noreply.github.com>
2025-03-30 06:13:06 +0000

    counter component to learn about client side and server side components

---
+++ app/components/Count.tsx
@@ -0,0 +1,10 @@
+"use client"
+import { useState } from "react";
+
+export default function Counter() {
+    console.log("Counter component");
+    const [count, setCount] = useState(0);
+    return (
+    <button onClick={() => setCount(count+1)}>Clicked {count} times</button>
+);
+}
---
+++ app/components/Greet.tsx
@@ -0,0 +1,6 @@
+export default function Greet(){
+    console.log("greet component");
+    return (
+        <h2>Greet Component</h2>
+    );
+}
