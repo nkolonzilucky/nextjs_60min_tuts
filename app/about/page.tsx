9388929023ff064df56603c4174246b474effa3c
Lucky Nkolonzi <35879963+nkolonzilucky@users.noreply.github.com>
2025-03-30 07:22:34 +0000

    demo how to use useRouter to access the router.push method that can help you navigate to a page programmatically

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,12 @@
+"use client"
+import { useRouter } from "next/navigation"
+
 export default function About() {
-    return <h1> About Us</h1>
+    const router = useRouter();
+    return (
+        <div>
+            <h1> About Us</h1>
+            <button onClick={() => router.push("/")} >Go Home</button>
+        </div>
+    );
 }
