d5e712b5ac5816af0fc2e76be006659eb640ffe2
Lucky Nkolonzi <35879963+nkolonzilucky@users.noreply.github.com>
2025-03-31 04:02:17 +0000

    added a add user form

--- app/mock-users/page.tsx
+++ app/mock-users/page.tsx
@@ -10,11 +10,20 @@ export default async function MockUsers(){
     const users = await res.json();
 
     return (
-        <ul className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-4 space-y-4 p-4">
-            {users.map((user: User) => {
-                return <li key={user.id} className="p-6 bg-white rounded-lg text-gray-700">{user.name}</li>
-            })}
-
-        </ul>
+        <div className="py-10 px-4">
+            <form className="mb-6">
+                <input name="name"
+                type="text"
+                placeholder="Enter the name"
+                className="border pl-6 pr-38 py-2 mr-2 rounded-lg text-left"
+                required />
+                <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-lg ">Add user</button>
+            </form>
+            <ul className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-4 space-y-4">
+                {users.map((user: User) => {
+                    return <li key={user.id} className="p-6 bg-white rounded-lg text-gray-700">{user.name}</li>
+                })}
+            </ul>
+        </div>
     );
 }
