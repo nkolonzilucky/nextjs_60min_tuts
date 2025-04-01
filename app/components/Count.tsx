7ec03c51dce6eeb5a7086e69c5d2d4b5954aa90c
Lucky Nkolonzi <35879963+nkolonzilucky@users.noreply.github.com>
2025-04-01 10:50:03 +0000

    Added auth info from client component: Counter

--- app/components/Count.tsx
+++ app/components/Count.tsx
@@ -1,9 +1,16 @@
 "use client"
+import { useAuth, useUser } from "@clerk/nextjs";
 import { useState } from "react";
 
 export default function Counter() {
+    // const { isLoaded, userId, sessionId, getToken } = useAuth();
+    const { isLoaded, isSignedIn, user } = useUser();
+
     console.log("Counter component");
     const [count, setCount] = useState(0);
+    if(!isLoaded || !isSignedIn) {
+        return null;
+    }    
     return (
     <button onClick={() => setCount(count+1)}>Clicked {count} times</button>
 );
